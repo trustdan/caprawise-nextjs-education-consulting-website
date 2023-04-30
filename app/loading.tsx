c960a92f33838302a6f217bd15242c9389b034c7
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-04-30 15:59:28 -0400

    edit loading.tsx to center animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="flex space-x-2">
+    <div className="flex space-x-2 p-5 w-full h-[calc(100vh-64px)] justify-center items-center">
       {Array.from({ length: 3 }, (_, i) => (
         <div
           key={i}
-          className="w-4 h-4 rounded-full bg-black animate-pulse"
+          className="w-4 h-4 rounded-full bg-gray-500 animate-pulse"
           style={{ animationDelay: `${i * 200}ms`, animationDuration: '1s' }}
         />
       ))}
