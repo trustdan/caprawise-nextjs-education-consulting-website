6e64a52b0141c87f58f444dad4bbb7f5655016ec
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-04-30 18:30:02 -0400

    remove use client, import wrapper component for SSR, add meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Header from "./components/Header";
 import { LanguageProvider } from "./contexts/LanguageContext";
+import ThemeProviders from "./components/ThemeProvider";
+import ThemeChanger from "./components/ThemeChanger";
 
 export const metadata = {
   title: "Education Consulting",
@@ -11,10 +13,13 @@ export default function RootLayout({children}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
       <body>
+      <ThemeProviders>
       <LanguageProvider >
         <Header />
+        <ThemeChanger />
         {children}
       </LanguageProvider>
+      </ThemeProviders>
       </body>
     </html>
   );
