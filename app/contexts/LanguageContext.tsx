8bff734dd4ed342903726727172f88c856d48b3d
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-04-30 15:56:48 -0400

    edit context with useMemo and localStorage

--- app/contexts/LanguageContext.tsx
+++ app/contexts/LanguageContext.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useMemo, useEffect } from "react";
 
 export interface LanguageContextValue {
   language: string;
@@ -15,10 +15,21 @@ interface LanguageProviderProps {
 }
 
 function LanguageProvider({ children }: LanguageProviderProps) {
-  const [language, setLanguage] = useState("en");
+  const [language, setLanguage] = useState<string>(() => {
+    // Get the language from localStorage on first render (this is for retaining the language value after the page refreshes)
+    const storedLanguage = window.localStorage.getItem("language");
+    return storedLanguage ?? "en";
+  });
+
+  const value = useMemo(() => ({ language, setLanguage }), [language]);
+
+  // Update the localStorage value whenever the language changes
+  useEffect(() => {
+    window.localStorage.setItem("language", language);
+  }, [language]);
 
   return (
-    <LanguageContext.Provider value={{ language, setLanguage }}>
+    <LanguageContext.Provider value={value}>
       {children}
     </LanguageContext.Provider>
   );
