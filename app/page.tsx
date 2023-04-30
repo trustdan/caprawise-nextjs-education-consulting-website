cee40e08a050e6bf0e9ebd2c6b6e3ef0c7a7918b
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-04-30 18:29:16 -0400

    add meta tags for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+export const metadata = {
+  title: "FAQ | SSS",
+  description: "Frequently Asked Questions | Sıkça Sorulan Sorular",
+};
+
 export default function Home() {
+
   return (
     <main className="flex flex-col bg-[yellow] justify-center items-center ">
         <h1>this is the homepage</h1>
