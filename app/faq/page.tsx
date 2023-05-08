0057b1e47f9caeb7819bc45e191e45c7c5124fd2
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-08 18:33:41 -0400

    add faq page filled with QA components

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,8 +1,70 @@
+import QA from "../components/QA";
+import PageIntroduction from "../components/PageIntroduction";
+
+export const metadata = {
+  title: "FAQ | SSS",
+  description: "Frequently Asked Questions | Sıkça Sorulan Sorular",
+};
+
 export default function FAQ() {
+  const FAQ_PAGE_INTRO = {
+    title: {
+      en: "Frequently Asked Questions",
+      tr: "Sıkça Sorulan Sorular",
+    },
+    description: {
+      en: "Here are some of the questions you may have and their answers.",
+      tr: "Aklınıza gelebilecek soruların bazıları ve cevapları",
+    },
+  };
+
+  const FAQ_DATA = () => [
+    {
+      question: {
+        en: "What is React?",
+        tr: "React nedir?",
+      },
+      answer: {
+        en: "React is a JavaScript library for building user interfaces.",
+        tr: "React, kullanıcı arayüzleri oluşturmak için kullanılan bir JavaScript kütüphanesidir.",
+      },
+    },
+    {
+      question: {
+        en: "What is Next.js?",
+        tr: "Next.js nedir?",
+      },
+      answer: {
+        en: "Next.js is a React framework for production.",
+        tr: "Next.js, üretim için bir React çatısıdır.",
+      },
+    },
+    {
+      question: {
+        en: "What is Tailwind CSS?",
+        tr: "Tailwind CSS nedir?",
+      },
+      answer: {
+        en: "Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.",
+        tr: "Tailwind CSS, özel kullanıcı arayüzleri oluşturmak için hızlı bir şekilde kullanılan bir CSS çatısıdır.",
+      },
+    },
+  ];
   return (
-    <main>
-      <h1>FAQ</h1>
-      <p>This is the FAQ page</p>
+    <main className="flex flex-col items-center bg-LIGHT_PRIMARY_BG_COLOR dark:bg-DARK_PRIMARY_BG_COLOR">
+      <PageIntroduction
+        title={FAQ_PAGE_INTRO.title}
+        description={FAQ_PAGE_INTRO.description}
+      />
+      <div className="flex flex-col w-full max-w-3xl ">
+        {FAQ_DATA().map((faq) => (
+          <QA
+            key={faq.question.en}
+            question={faq.question}
+            answer={faq.answer}
+          />
+        ))}
+      </div>
     </main>
   );
 }
