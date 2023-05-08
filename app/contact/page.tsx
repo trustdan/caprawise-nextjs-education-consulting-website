d3b0cefc9d81f29e7d6e0e33eeb35e4ce6dfede1
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-08 18:32:04 -0400

    add placeholder for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,11 @@
+export const metadata = {
+  title: "Contact | İletişim",
+  description: "Contact us | Bize ulaşın",
+};
+
 export default function Contact() {
   return (
-    <main>
+    <main className="bg-LIGHT_PRIMARY_BG_COLOR dark:bg-DARK_PRIMARY_BG_COLOR">
       <h1>Contact</h1>
       <p>This is the contact page</p>
     </main>
