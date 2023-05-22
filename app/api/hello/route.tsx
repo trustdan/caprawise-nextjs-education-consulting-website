7ed6c7fd19ad0c0a03ded0cc81f702c662ab0685
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-22 00:26:02 -0400

    add submit-contact-form api route

--- app/api/hello/route.tsx
+++ app/api/hello/route.tsx
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: Request) {
   const resp = [
     {
-      "name": "John",
-      "age": 30,
-      "car": "bmw"
+      name: "John",
+      age: 30,
+      car: "bmw",
     },
     {
-      "name": "weq",
-      "age": 302,
-      "car": "ferrari"
+      name: "weq",
+      age: 302,
+      car: "ferrari", 
     },
-  ]
+  ];
   return new Response(JSON.stringify(resp));
 }
