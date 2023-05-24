137ff35d5c378b97b66f9564684cd9c5143a1d01
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-24 02:22:44 -0400

    edit config files, tailwind config, next config, page.tsx, loading.tsx, globals.css and more

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,43 @@
+import React from "react";
 import "./globals.css";
 import Header from "./components/Header";
-import { LanguageProvider } from "./contexts/LanguageContext";
+import { LanguageProvider } from "./contexts/LanguageProvider";
+import ThemeProviders from "./contexts/ThemeProvider";
+import ThemeChanger from "./contexts/ThemeChanger";
+import Footer from "./components/Footer";
+import { Nunito_Sans, Nunito } from "next/font/google";
+import QueryProvider from "./contexts/QueryProvider";
 
 export const metadata = {
   title: "Education Consulting",
-  description: "Official website of  XXX Education Consulting Company based in the United States",
+  description:
+    "Official website of  XXX Education Consulting Company based in the United States",
 };
 
-export default function RootLayout({children}: {children: React.ReactNode;}) {
-  
+const nunito = Nunito({
+  weight: ["200", "300", "400", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin-ext", "latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-      <LanguageProvider >
-        <Header />
-        {children}
-      </LanguageProvider>
+      <body className={nunito.className}>
+        <ThemeProviders>
+          <QueryProvider>
+            <LanguageProvider>
+              <Header />
+              <ThemeChanger />
+              {children}
+              <Footer />
+            </LanguageProvider>
+          </QueryProvider>
+        </ThemeProviders>
       </body>
     </html>
   );
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
+import PageStyler from "./components/PageStyler";
 
 const Loading = () => {
   return (
-    <div className="flex space-x-2">
-      {Array.from({ length: 3 }, (_, i) => (
-        <div
-          key={i}
-          className="w-4 h-4 rounded-full bg-black animate-pulse"
-          style={{ animationDelay: `${i * 200}ms`, animationDuration: '1s' }}
-        />
-      ))}
-    </div>
+    <PageStyler>
+      <main className="flex space-x-2 p-5 w-full justify-center items-center">
+        {Array.from({ length: 3 }, (_, i) => (
+          <div
+            key={i}
+            className="w-4 h-4 rounded-full bg-gray-500 animate-pulse"
+            style={{ animationDelay: `${i * 200}ms`, animationDuration: "1s" }}
+          />
+        ))}
+      </main>
+    </PageStyler>
   );
 };
 
