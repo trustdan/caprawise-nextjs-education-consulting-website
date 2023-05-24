5bb8f8faeed3793a97265b9427fe540160ee0fe6
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-24 02:18:14 -0400

    refactor contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,36 @@
+import PageStyler from "../components/PageStyler";
+import Form from "../components/Form";
+import PageIntroduction from "../components/PageIntroduction";
+import RecaptchaProvider from "../components/RecaptchaProvider";
+
+export const metadata = {
+  title: "Contact | İletişim",
+  description: "Contact us | Bize ulaşın",
+};
+
+
 export default function Contact() {
+  const CONTACT_INTRODUCTION_DATA = {
+    title: {
+      en: "Any questions on your mind?",
+      tr: "Bizimle iletişime geçin",
+    },
+    description: {
+      en: "We look forward to hearing from you! Please fill out the form below and we will get back to you as soon as possible :)",
+      tr: "Aklınıza takılan konular için bizimle iletişime geçin. Aşağıdaki formu doldurduktan sonra keyfinize bakın ve bizden haber bekleyin :)",
+    },
+  };
   return (
-    <main>
-      <h1>Contact</h1>
-      <p>This is the contact page</p>
-    </main>
+    <PageStyler>
+      <main>
+        <RecaptchaProvider>
+          <PageIntroduction
+            title={CONTACT_INTRODUCTION_DATA.title}
+            description={CONTACT_INTRODUCTION_DATA.description}
+          />
+          <Form />
+        </RecaptchaProvider>
+      </main>
+    </PageStyler>
   );
 }
