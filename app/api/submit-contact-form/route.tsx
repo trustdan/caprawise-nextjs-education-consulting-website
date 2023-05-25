8da6099a195be40fade9891ea847ef1f07c46591
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-25 12:45:31 -0400

    merge form submission and email confirmation api routes into one

--- app/api/submit-contact-form/route.tsx
+++ app/api/submit-contact-form/route.tsx
@@ -1,96 +1,99 @@
 import { verifyRecaptcha } from "@/app/utilities/VerifyCaptcha";
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/app/lib/prisma";
+import { saveContactFormToDb } from "@/app/utilities/SaveContactFormToDb";
+import { sendEmailToHelios, sendEmailToUser } from "@/app/utilities/SendEmail";
 
 export async function POST(request: NextRequest) {
   if (request.method === "POST") {
     // Get the form data and the token from the request body
     const { formData, token } = await request.json();
     if (formData && token) {
-      try {
-        const recaptchaVerificationResponse = await verifyRecaptcha(token);
-        const recaptchaVerificationResponseJSON = await recaptchaVerificationResponse.json();
-
-        if (recaptchaVerificationResponseJSON.success) {
-          // Get the score field
-          const score = recaptchaVerificationResponseJSON.score;
-
-          if (score < 0.7) {
-            return new NextResponse(
-              JSON.stringify({
-                message: "Low reCAPTCHA score, please try again",
-              }),
-              {
-                status: 400,
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-          } else {
-            try {
-              const newContactFormSubmission = await prisma.contactForm.create({
-                data: {
-                  full_name: formData.fullName,
-                  email: formData.email,
-                  phone: formData.phone,
-                  message: formData.question,
-                },
-              });
-              console.log(newContactFormSubmission);
-              return new NextResponse(
-                JSON.stringify({ message: "Form submitted successfully" }),
-                {
-                  status: 200,
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              );
-            } catch (error) {
-              console.error(error);
-              return new NextResponse(
-                JSON.stringify({
-                  message: "Error while saving the form to the database",
-                }),
-                {
-                  status: 500,
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              );
-            }
-          }
-        } else {
-          // Handle failed reCAPTCHA verification
-          return new NextResponse(
-            JSON.stringify({
-              message: "reCAPTCHA verification failed",
-              errorCodes: recaptchaVerificationResponseJSON["error-codes"],
-            }),
-            {
-              status: 400,
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-        }
-      } catch (error) {
-        console.error(error);
+      // 1.verify captcha
+      const isCaptchaValid = await verifyRecaptcha(token);
+      console.log("captcha is ", isCaptchaValid);
+      if (!isCaptchaValid) {
         return new NextResponse(
           JSON.stringify({
-            message: "Failed to verify reCAPTCHA token",
+            message: "Failed to verify captcha",
           }),
           {
-            status: 500,
+            status: 400,
             headers: {
               "Content-Type": "application/json",
             },
           }
         );
       }
+      // 2. save form data to db
+      const isFormSaved = await saveContactFormToDb(formData);
+      console.log("Form saved?: ", isFormSaved);
+      if (!isFormSaved) {
+        return new NextResponse(
+          JSON.stringify({
+            message: "Failed to save form data to database",
+          }),
+          {
+            status: 400,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      // 3.a. send form data email to admin b. send "form received email" to the user
+      // 3a
+      const isEmailSentToAdmin = await sendEmailToHelios(
+        formData.fullName,
+        formData.email,
+        formData.phone,
+        formData.question
+      );
+      if (!isEmailSentToAdmin) {
+        return new NextResponse(
+          JSON.stringify({
+            message: "Failed to send email to admin",
+          }),
+          {
+            status: 400,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      //3b
+
+      const isEmailSentToUser = await sendEmailToUser(
+        formData.fullName,
+        formData.email
+      );
+      if (!isEmailSentToUser) {
+        return new NextResponse(
+          JSON.stringify({
+            message: "Failed to send email to user",
+          }),
+          {
+            status: 400,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      return new NextResponse(
+        JSON.stringify({
+          message: "Form submitted successfully",
+        }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
     }
   }
 }
