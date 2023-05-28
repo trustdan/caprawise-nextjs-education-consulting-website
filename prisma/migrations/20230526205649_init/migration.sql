e9c4200db9dbcda135a058f61beb35c2b9d57d27
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-28 19:30:26 -0400

    update prisma data model with User table and add a new migration

---
+++ prisma/migrations/20230526205649_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
