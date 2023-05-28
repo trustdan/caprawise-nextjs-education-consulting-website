24d0a00bd6f7b89152d76535d51780d956248828
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-05-28 19:21:12 -0400

    add JWT token & bcrypt hashing

---
+++ app/api/login/route.tsx
@@ -0,0 +1,37 @@
+import prisma from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { signJwtAccessToken } from "@/app/lib/jwt";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { username, password } = body;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username: username,
+    },
+  });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const { password, ...userWithoutPass } = user;
+    const accessToken = signJwtAccessToken(userWithoutPass);
+    const result = {
+      ...userWithoutPass,
+      accessToken,
+    };
+    return new NextResponse(JSON.stringify(result), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } else {
+    return new NextResponse(JSON.stringify(null), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
