0a6035f68b53fdfa2271cf5bc295610556433f62
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-01 14:23:48 -0400

    add application form records to the admin dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -2,14 +2,14 @@ import AdminMenu from "@/app/components/AdminMenu";
 import PageStyler from "@/app/components/PageStyler";
 
 export const metadata = {
-  title: "Admin Dashboard| Admin Paneli",
+  title: "Admin Dashboard | Admin Paneli",
   description: "Admin-only dashboard | Sadece yöneticilerin görebileceği panel",
 };
 
 export default function Dashboard() {
   return (
     <PageStyler>
-      <main className="md:p-10 p-4  max-w-full ">
+      <main className="md:p-10 p-4 max-w-full ">
         <AdminMenu />
       </main>
     </PageStyler>
