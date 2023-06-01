257df0fb85ef28b71e7eac09fa098e9126d37712
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-01 14:27:14 -0400

    fix dark mode colors for AdminLogin component

--- app/components/AdminLogin.tsx
+++ app/components/AdminLogin.tsx
@@ -1,17 +1,16 @@
 "use client";
 import PrimaryButton from "../components/PrimaryButton";
 import { useAdminLogin } from "../hooks/useAdminLogin";
-
 export default function AdminLogin() {
-  const {usernameRef, passwordRef, handleAdminLogin} = useAdminLogin();
+  const { usernameRef, passwordRef, handleAdminLogin } = useAdminLogin();
 
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-270px)]">
-      <div className="flex flex-col items-center justify-center w-96 h-96 bg-white rounded-xl shadow-xl gap-10">
-        <h1 className="text-3xl font-bold dark:text-white">Admin Login</h1>
+      <div className="flex flex-col items-center justify-center w-96 h-96 bg-white rounded-xl shadow-xl gap-10 dark:text-black">
+        <h1 className="text-3xl font-bold ">Admin Login</h1>
         <form className="flex flex-col items-center justify-center gap-8">
           <input
-            className="w-full h-10 px-3  text-lg border-2 border-gray-300 rounded-xl focus:outline-none focus:border-blue-500"
+            className="w-full h-10 px-3 text-lg border-2 border-gray-300 rounded-xl focus:outline-none focus:border-blue-500 dark:bg-black dark:text-white "
             type="text"
             placeholder="Username"
             onChange={(e) => {
@@ -19,7 +18,7 @@ export default function AdminLogin() {
             }}
           />
           <input
-            className="w-full h-10 px-3 text-lg border-2 border-gray-300 rounded-xl focus:outline-none focus:border-blue-500"
+            className="w-full h-10 px-3 text-lg border-2 border-gray-300 rounded-xl focus:outline-none focus:border-blue-500 dark:bg-black dark:text-white"
             type="password"
             placeholder="Password"
             onChange={(e) => {
