c9fad06a37e4e521838285b6588ab34c4e7958c5
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-24 15:47:14 -0400

    edit form attributes

--- app/components/AdminLogin.tsx
+++ app/components/AdminLogin.tsx
@@ -8,7 +8,11 @@ export default function AdminLogin() {
     <div className="flex flex-col items-center justify-center h-[calc(100vh-270px)]">
       <div className="flex flex-col items-center justify-center w-96 h-96 bg-white rounded-xl shadow-xl gap-10 dark:text-black">
         <h1 className="text-3xl font-bold ">Admin Login</h1>
-        <form className="flex flex-col items-center justify-center gap-8">
+        <form
+          className="flex flex-col items-center justify-center gap-8"
+          method="POST"
+          onSubmit={handleAdminLogin}
+        >
           <input
             className="w-full h-10 px-3 text-lg border-2 border-gray-300 rounded-xl focus:outline-none focus:border-blue-500 dark:bg-black dark:text-white "
             type="text"
@@ -30,7 +34,6 @@ export default function AdminLogin() {
             label={{ en: "login", tr: "login" }}
             className="text-white w-[270px] h-10  bg-blue-500 rounded-xl hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
             type="submit"
-            onClick={handleAdminLogin}
           />
         </form>
       </div>
