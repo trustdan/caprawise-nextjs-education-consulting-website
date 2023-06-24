48aa8fd075bd504ce4fdbd0d81d9b54d571d5695
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-24 16:25:04 -0400

    update package-lock.json, package.json and tailwind.config.js with new packages and modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-pseudo-left":
+          "linear-gradient(to left, rgba(242,236,228,0) 0%, rgba(242,236,228,1) 100%)",
+        "gradient-pseudo-right":
+          "linear-gradient(to right, rgba(242,236,228,0) 0%, rgba(242,236,228,1) 100%)",
+        "gradient-pseudo-right-dark":
+          "linear-gradient(to right, rgba(17,24,39,0) 0%, rgba(17,24,39,1) 100%)",
+        "gradient-pseudo-left-dark":
+          "linear-gradient(to left, rgba(17,24,39,0) 0%, rgba(17,24,39,1) 100%)",
       },
       backgroundColor: {
         LIGHT_PRIMARY_BG_COLOR: "#f2ece4",
@@ -21,10 +29,27 @@ module.exports = {
         DARK_PRIMARY_BG_COLOR: "#111827",
         DARK_SECONDARY_BG_COLOR: "#1F2937",
       },
+      minHeight: {
+        PAGE_MIN_HEIGHT: "calc(100vh - 12rem)",
+      },
+      keyframes: {
+        shift: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        shift: "shift 15s  infinite linear",
+      },
     },
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: false,
+  },
+  variants: {
+    extend: {
+      backgroundClip: ["responsive"],
+    },
   },
 };
