5c007642147581d3eab56e747386dd2c0ef6add7
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-25 00:19:16 -0400

    add gsap effect with stagger

--- app/hooks/useQA.tsx
+++ app/hooks/useQA.tsx
@@ -2,25 +2,26 @@ import { useLayoutEffect, useRef, useState } from "react";
 import { useLanguageContext } from "./useLanguageContext";
 import { gsap } from "gsap";
 export default function useQA() {
-
-  const QARef = useRef<any>(null);
-
   useLayoutEffect(() => {
-    // GSAP Animations  
+    // GSAP Animations
 
-    const tl = gsap.timeline();
-    
-    tl.from(QARef.current, {
-      y: 100,
-      opacity: 0,
-      duration: 1,
-      ease: "power3.out",
-    });
+    gsap.fromTo(
+      ".gsap-qa",
+      {
+        opacity: 0,
+        y: 100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        stagger: 0.08,
+        ease: "easeInOut",
+      }
+    );
   }, []);
 
   const [isOpen, setIsOpen] = useState(false);
   const language = useLanguageContext().language;
 
-  return { isOpen, setIsOpen, language, QARef };
+  return { isOpen, setIsOpen, language };
 }
-
