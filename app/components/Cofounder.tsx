6667da6dae2a19d65f35d1193c6e6902d46b98ff
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-26 16:36:27 -0400

    add priority:true property to next/image

--- app/components/Cofounder.tsx
+++ app/components/Cofounder.tsx
@@ -28,6 +28,7 @@ export default function Cofounder(props: CofounderProps) {
         alt={props.imageAlt}
         width={250}
         height={250}
+        priority={true}
       />
       <div className="flex flex-col gap-2 lg:gap-5 leading-[1.42]">
         <div className="flex flex-col text-xl text-center lg:text-left lg:mt-[-5px] ">
