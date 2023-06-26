3eec1ed0a2c0b6f807b8be4acdf896daba12e0e3
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-06-26 16:37:53 -0400

    add min-h property to avoid flashing when images are prefetched

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -27,7 +27,7 @@ export default function About() {
       name: "Oğuzhan Yangöz",
       title: "Co-founder",
       description: {
-        en: "Yangöz has a bachelor’s and master’s degree in Foreign Language Education from Yıldız Technical University. After working as a lecturer for three years, he was awarded a Fulbright scholarship and worked as a teaching assistant at Yale University where he also took graduate-level courses in Computer Science and Data Science. After completing the Fulbright program, he completed his second Master's Degree in Computer Science with focus on Cognition and Machine Intelligence at George Washington University. His areas of interest include Distributed Systems and Cloud Computing, and he has helped many of his students with their graduate studies abroad since the beginning of his career.",
+        en: "Yangöz has a Bachelor’s and Master’s Degree in Foreign Language Education from Yildız Technical University. After working as a lecturer for three years, he was awarded a Fulbright scholarship and worked as a teaching assistant at Yale University where he also took graduate-level courses in Computer Science and Data Science. After completing the Fulbright program, he completed his second Master's Degree in Computer Science with focus on Cognition and Machine Intelligence at George Washington University. His areas of interest include Distributed Systems and Cloud Computing, and he has helped many of his students with their graduate studies abroad since the beginning of his career.",
         tr: "Yıldız Teknik Üniversitesi’nden Yabancı Dil Eğitimi alanında lisans ve yüksek lisans diploması bulunan Yangöz, üç yıl boyunca öğretim görevlisi olarak çalıştıktan sonra Fulbright bursiyeri olarak Yale Üniversitesi’nde asistanlık yapmış, aynı zamanda Bilgisayar Bilimleri ve Veri Bilimi alanlarında yüksek lisans seviyesinde dersler almıştır. Fulbright programını tamamladıktan sonra ise, George Washington Üniversitesi’nde Bilgisayar Bilimleri alanında Makine Öğrenmesi ve Doğal Dil İşleme alanlarına yoğunlaşarak yüksek lisansını tamamlamıştır. İlgi alanları Dağınık Sistemler (Distributed Systems) ve Bulut Bilişim (Cloud Computing) olup, öğretim görevliliğinden günümüze kadar birçok öğrencisine yurtdışında lisansüstü eğitim konusunda yardımcı olmuştur. ",
       },
       imagePath: "/oguzhan.webp",
@@ -48,7 +48,10 @@ export default function About() {
 
   return (
     <PageStyler>
-      <section id="our-team" className="h-full px-5 lg:pb-8 pb-5">
+      <section
+        id="our-team"
+        className="h-full px-5 lg:pb-8 pb-5 min-h-[100svh-5rem]"
+      >
         <PageIntroduction
           title={ABOUT_PAGE_INTRODUCTION.title}
           description={ABOUT_PAGE_INTRODUCTION.description}
