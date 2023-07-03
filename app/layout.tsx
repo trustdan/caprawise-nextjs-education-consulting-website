931452f9cb535e016f77edc11b5e7d0c981921cb
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-03 00:36:32 -0400

    add favicon links to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,30 @@ export const metadata = {
     "Helios Admissions - Amerika'da Yüksek lisans, Doktora ve Dil Okulu Danışmanlığı | Helios Admissions - Graduate School, PhD and Language School Consulting in the United States ",
   description:
     "ABD merkezli Helios Admissions Resmi Web Sitesi | Official website of Helios Education Consulting Company based in the United States",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      rel: "manifest",
+      url: "/favicon/site.webmanifest",
+    },
+  ],
 };
 
 const nunito_sans = Nunito_Sans({
