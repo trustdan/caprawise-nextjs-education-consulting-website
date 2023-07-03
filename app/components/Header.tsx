23e98be6719c42173e4b2476676ddad7bd6f3142
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-03 03:21:11 -0400

    update Header.tsx with Logo and edit flex-order for different screens

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,80 +5,71 @@ import { useHeader } from "../hooks/useHeader";
 import { LanguageDropdown } from "./LanguageDropdown";
 import { signOut, useSession } from "next-auth/react";
 import PrimaryButton from "./PrimaryButton";
+import Logo from "./Logo";
 
 export default function Header() {
-  const {
-    language,
-    NAV_BAR_ITEMS,
-    activeLink,
-    setActiveLink,
-    headerRef,
-    navItemsRef,
-    languageRef,
-    hamburgerRef,
-  } = useHeader();
-
+  const { language, NAV_BAR_ITEMS, activeLink, setActiveLink, headerRef } = useHeader();
   const { data: session } = useSession();
 
   return (
     <>
       <header
-        className="-translate-y-24 sticky top-0 z-50 bg-LIGHT_SECONDARY_BG_COLOR"
+        className="opacity-0 -translate-y-24 sticky top-0 z-50 bg-LIGHT_SECONDARY_BG_COLOR"
         ref={headerRef}
       >
         <div className="flex h-16 max-w-screen-3xl px-5">
-          <div className="flex flex-1 items-center justify-end gap-4">
-            <nav
-              className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-widest opacity-0"
-              ref={navItemsRef}
-            >
-              {NAV_BAR_ITEMS.map((item) => (
-                <Link
-                  key={item.href}
-                  href={item.href}
-                  className={`block h-16 border-b-4 leading-[4rem] transition ease-in-out duration-150 active:scale-90 text-[#22577E] hover:border-[#22577E] ${
-                    activeLink === item.href
-                      ? "border-[#22577E] "
-                      : "border-transparent "
-                  }`}
-                  onClick={() => setActiveLink(item.href)}
-                >
-                  {language === "en" ? item.name.en : item.name.tr}
-                </Link>
-              ))}
-
-              {session && (
-                <Link
-                  href="/admin/dashboard"
-                  className={`hidden lg:block h-16 border-b-4 leading-[4rem] transition ease-in-out duration-150 active:scale-90 text-[#22577E] hover:border-[#22577E] ${
-                    activeLink === "/admin/dashboard"
-                      ? "border-[#22577E] "
-                      : "border-transparent "
-                  }`}
-                  onClick={() => setActiveLink("/admin/dashboard")}
-                >
-                  {language === "en" ? "DASHBOARD" : "ADMİN PANELİ"}
-                </Link>
-              )}
-            </nav>
-            <div
-              role="div for gsap effect"
-              className="opacity-0 fixed left-5 lg:relative lg:left-0"
-              ref={languageRef}
-            >
-              <LanguageDropdown />
+          <div className="flex flex-1 items-center justify-between gap-4">
+            <div className="order-2 lg:order-1">
+              <Logo setActiveLink={setActiveLink} />
             </div>
+
+            <div className="lg:flex lg:items-center lg:gap-4 lg:order-2">
+              <nav className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-widest">
+                {NAV_BAR_ITEMS.map((item) => (
+                  <Link
+                    key={item.href}
+                    href={item.href}
+                    className={`block h-16 border-b-4 leading-[4rem] transition ease-in-out duration-150 active:scale-90 text-[#22577E] hover:border-[#22577E] ${
+                      activeLink === item.href
+                        ? "border-[#22577E] "
+                        : "border-transparent "
+                    }`}
+                    onClick={() => setActiveLink(item.href)}
+                  >
+                    {language === "en" ? item.name.en : item.name.tr}
+                  </Link>
+                ))}
+
+                {session && (
+                  <Link
+                    href="/admin/dashboard"
+                    className={`hidden lg:block h-16 border-b-4 leading-[4rem] transition ease-in-out duration-150 active:scale-90 text-[#22577E] hover:border-[#22577E] ${
+                      activeLink === "/admin/dashboard"
+                        ? "border-[#22577E] "
+                        : "border-transparent "
+                    }`}
+                    onClick={() => setActiveLink("/admin/dashboard")}
+                  >
+                    {language === "en" ? "DASHBOARD" : "ADMİN PANELİ"}
+                  </Link>
+                )}
+              </nav>
+
+              <div role="div for gsap effect" className="order-1 lg:order-2">
+                <LanguageDropdown />
+              </div>
+            </div>
+
             {session && (
               <PrimaryButton
                 label={{ en: "Log out", tr: "Çıkış Yap" }}
                 className="text-white hidden lg:block text-xs font-bold uppercase tracking-widest "
                 onClick={() => signOut()}
               />
-            )}  
+            )}
             <div
               role="div for gsap effect"
-              className="opacity-0 lg:hidden"
-              ref={hamburgerRef}
+              className="lg:hidden order-3 lg:order-none"
             >
               <Hamburger
                 navBarItems={NAV_BAR_ITEMS}
