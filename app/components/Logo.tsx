6d8db0fffd964d7c05660d07bb96ac6905b83cd3
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-09 22:39:35 -0400

    remove helios image logo temporarily to only use text logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -9,15 +9,6 @@ type LogoProps = {
 export default function Logo(props: LogoProps) {
   return (
     <section className="flex gap-4 items-center ">
-      <Image
-        src="/logo.webp"
-        alt="Helios Admissions Logo"
-        width={60}
-        height={60}
-        priority={true}
-        quality={100}
-        className="hidden lg:block"
-      />
       <Link href="/" onClick={() => props.setActiveLink("/")}>
         <h1 className="text-sm lg:text-md font-sans uppercase tracking-widest hover:cursor-pointer hover:text-gray-500 transition-all duration-200">
           Helios Admissions
